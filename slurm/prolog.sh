a20f0c64c14f2f00d6042a7356be63f020567aea
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-03-03 09:44:05 -0800

    prolog doesnt need sudo if run as TaskProlog

--- slurm/prolog.sh
+++ slurm/prolog.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-/usr/bin/sudo \
-	--preserve-env=SLURM_JOB_NODELIST \
-	--preserve-env=SLURM_NTASKS \
-	--user=ci \
-	/home/ci/scopehal-ci-scripts/vm/spawn-vm > /tmp/prolog.log 2>&1
+/home/ci/scopehal-ci-scripts/vm/spawn-vm > /tmp/prolog.log 2>&1
