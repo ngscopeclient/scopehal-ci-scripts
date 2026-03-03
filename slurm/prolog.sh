1c1615e98aeb6c3b5bf80ff7704bf8f215b75e2b
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-03-03 09:41:02 -0800

    Convert prolog to taskprolog so we can get the needed environment

--- slurm/prolog.sh
+++ slurm/prolog.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "NODELIST=$SLURM_JOB_NODELIST NTASKS=$SLURM_NTASKS"
 /usr/bin/sudo \
-	--preserve-env=SLURM_JOB_NODELIST,SLURM_NTASKS \
+	--preserve-env=SLURM_JOB_NODELIST \
+	--preserve-env=SLURM_NTASKS \
 	--user=ci \
 	/home/ci/scopehal-ci-scripts/vm/spawn-vm > /tmp/prolog.log 2>&1
