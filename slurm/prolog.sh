19e09ba1df0dacfe023b9959b8c88ae754e0c3cb
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-03-03 09:37:39 -0800

    preserve slurm job env in prolog

--- slurm/prolog.sh
+++ slurm/prolog.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/usr/bin/sudo --user=ci /home/ci/scopehal-ci-scripts/vm/spawn-vm > /tmp/prolog.log 2>&1
+echo "NODELIST=$SLURM_JOB_NODELIST NTASKS=$SLURM_NTASKS"
+/usr/bin/sudo \
+	--preserve-env=SLURM_JOB_NODELIST,SLURM_NTASKS \
+	--user=ci \
+	/home/ci/scopehal-ci-scripts/vm/spawn-vm > /tmp/prolog.log 2>&1
