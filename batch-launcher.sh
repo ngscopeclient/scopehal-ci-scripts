433821652084d3543bac6cbd026a8c224470f5da
Andrew Zonenberg <azonenberg@drawersteak.com>
2026-03-05 16:32:46 -0800

    Added nvidia request to batch launcher script

--- batch-launcher.sh
+++ batch-launcher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sbatch -p debian-stable run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
-sbatch -p debian-oldstable run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
-sbatch -p ubuntu-oldlts run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
+sbatch -L nvidia1630:1 -p debian-stable run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
+sbatch -L nvidia1630:1 -p debian-oldstable run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
+sbatch -L nvidia1630:1 -p ubuntu-oldlts run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
