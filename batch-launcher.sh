532e54ebf2db6f46cd5dc816cfd4668e7d4ac095
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-03-11 15:06:37 -0700

    Updated build config for debian stable VM, allow slurm to request specific passthrough GPUs

--- batch-launcher.sh
+++ batch-launcher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sbatch -L nvidia1630:1 -p debian-stable run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
+sbatch -L nvidia1630:1,nvidia1630_18:1 -p debian-stable run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
 sbatch -L nvidia1630:1 -p debian-oldstable run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
 sbatch -L nvidia1630:1 -p ubuntu-oldlts run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
