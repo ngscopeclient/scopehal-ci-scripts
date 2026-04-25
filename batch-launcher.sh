9707f6f03a436782726b08b61dd1eb90aeb10227
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-04-24 23:39:36 -0700

    Fixed broken run script

--- batch-launcher.sh
+++ batch-launcher.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
-sbatch -L nvidia1630:1,nvidia1630_8a:1 -p debian-stable run-task scopehal-ci-scripts/ci-jobs/job-debian.sh -o "run-logs/slurm-%j.out"
-sbatch -L nvidia1630:1,nvidia1630_51:1 -p debian-oldstable run-task scopehal-ci-scripts/ci-jobs/job-debian.sh -o "run-logs/slurm-%j.out"
-sbatch -L nvidia1630:1,nvidia1630_18:1 -p ubuntu-oldlts run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh -o "run-logs/slurm-%j.out"
-sbatch                                 -p ubuntu-lts run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh -o "run-logs/slurm-%j.out"
+
+# Build and run tests with GPU
+sbatch -o "run-logs/slurm-%j.out" \
+	-L nvidia1630:1,nvidia1630_8a:1 \
+	-p debian-stable \
+	run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
+sbatch -o "run-logs/slurm-%j.out" \
+	-L nvidia1630:1,nvidia1630_51:1 \
+	-p debian-oldstable \
+	run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
+sbatch -o "run-logs/slurm-%j.out" \
+	-L nvidia1630:1,nvidia1630_18:1 \
+	-p ubuntu-oldlts \
+	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh
+
+# Build and run tests with no GPU
+sbatch -o "run-logs/slurm-%j.out" \
+	-p ubuntu-lts \
+	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh
