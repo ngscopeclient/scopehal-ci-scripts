e0120c99cc0536129d7c0734c718ad3e561a40be
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-04-25 05:12:49 -0700

    Windows VM now has GPU

--- batch-launcher.sh
+++ batch-launcher.sh
@@ -14,14 +14,14 @@ sbatch -o "run-logs/slurm-%j.out" \
 	-p ubuntu-oldlts \
 	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh
 
+sbatch -o "run-logs/slurm-%j.out" \
+	-L nvidia1630:1,nvidia1630_18:1 \
+	-p win11 \
+	run-task-msys scopehal-ci-scripts/ci-jobs/job-windows.sh
+
 # Build and run tests with no GPU
 # These request the "nogpu" license so we can manage oversubscription
 sbatch -o "run-logs/slurm-%j.out" \
 	-L nogpu:1 \
 	-p ubuntu-lts \
 	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh
-
-sbatch -o "run-logs/slurm-%j.out" \
-	-L nogpu:1 \
-	-p win11 \
-	run-task-msys scopehal-ci-scripts/ci-jobs/job-windows.sh
