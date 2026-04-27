54e09f11bea5076a522d56e8ea55ec56aef4d123
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-04-27 01:25:16 -0700

    Initial macos ci job

--- batch-launcher.sh
+++ batch-launcher.sh
@@ -34,3 +34,11 @@ sbatch -o "run-logs/slurm-%j.out" \
 	-L nogpu:1 \
 	-p fedora \
 	run-task scopehal-ci-scripts/ci-jobs/job-fedora.sh
+
+# Build and run tests that execute on the Mac Mini
+# These request the "macmini" license so we can limit the number of jobs on the host,
+# which has both a hypervisor-enforced 2-instance license limit, and only 16GB of RAM
+sbatch -o "run-logs/slurm-%j.out" \
+	-L macmini:1 \
+	-p macos\
+	run-task scopehal-ci-scripts/ci-jobs/job-macos.sh
