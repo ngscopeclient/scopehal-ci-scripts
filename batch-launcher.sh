7b3aa24f7e053720e2a69256f95cdd8d7d7b9bce
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-04-29 02:14:25 -0700

    Updated for new NIC configuration, reshuffled GPU allocations to reduce conflicts

--- batch-launcher.sh
+++ batch-launcher.sh
@@ -1,28 +1,31 @@
 #!/bin/sh
 
+########################################################################################################################
 # Build and run tests using GPUs
-sbatch -o "run-logs/slurm-%j.out" \
-	-L nvidia1630:1,nvidia1630_8a:1 \
-	-p debian-stable \
-	run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
-sbatch -o "run-logs/slurm-%j.out" \
-	-L nvidia1630:1,nvidia1630_51:1 \
-	-p debian-oldstable \
-	run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
-sbatch -o "run-logs/slurm-%j.out" \
-	-L nvidia1630:1,nvidia1630_18:1 \
-	-p ubuntu-oldlts \
-	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh
-
+# Submits are grouped in blocks of up to 3, one per GPU. for clarity
 sbatch -o "run-logs/slurm-%j.out" \
 	-L nvidia1630:1,nvidia1630_18:1 \
 	-p win11 \
 	run-task-msys scopehal-ci-scripts/ci-jobs/job-windows.sh
 sbatch -o "run-logs/slurm-%j.out" \
 	-L nvidia1630:1,nvidia1630_51:1 \
+	-p debian-oldstable \
+	run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
+sbatch -o "run-logs/slurm-%j.out" \
+	-L nvidia1630:1,nvidia1630_8a:1 \
+	-p ubuntu-oldlts \
+	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh
+
+sbatch -o "run-logs/slurm-%j.out" \
+	-L nvidia1630:1,nvidia1630_51:1 \
 	-p arch \
 	run-task scopehal-ci-scripts/ci-jobs/job-arch.sh
+sbatch -o "run-logs/slurm-%j.out" \
+	-L nvidia1630:1,nvidia1630_8a:1 \
+	-p debian-stable \
+	run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
 
+########################################################################################################################
 # Build and run tests with no GPU
 # These request the "nogpu" license so we can manage oversubscription
 sbatch -o "run-logs/slurm-%j.out" \
@@ -35,6 +38,7 @@ sbatch -o "run-logs/slurm-%j.out" \
 	-p fedora \
 	run-task scopehal-ci-scripts/ci-jobs/job-fedora.sh
 
+########################################################################################################################
 # Build and run tests that execute on the Mac Mini
 # These request the "macmini" license so we can limit the number of jobs on the host,
 # which has both a hypervisor-enforced 2-instance license limit, and only 16GB of RAM
