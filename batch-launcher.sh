b6bb5f8e8716960543c9fd93329f82abca9e52ea
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-05-07 05:35:26 -0700

    Initial sanitizer flow

--- batch-launcher.sh
+++ batch-launcher.sh
@@ -2,7 +2,10 @@
 
 ########################################################################################################################
 # Build and run tests using GPUs
-# Submits are grouped in blocks of up to 3, one per GPU. for clarity
+# Submits are grouped in blocks of up to 3, one per GPU, for clarity but are scheduled by SLURM
+# so jobs from a future block can begin running once the job on their GPU from the previous block completes,
+# without any need to wait for other jobs in the block
+
 sbatch -o "run-logs/slurm-%j.out" \
 	-L nvidia1630:1,nvidia1630_18:1 \
 	-p win11 \
@@ -16,6 +19,8 @@ sbatch -o "run-logs/slurm-%j.out" \
 	-p ubuntu-oldlts \
 	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh
 
+##
+
 sbatch -o "run-logs/slurm-%j.out" \
 	-L nvidia1630:1,nvidia1630_51:1 \
 	-p arch \
@@ -27,11 +32,17 @@ sbatch -o "run-logs/slurm-%j.out" \
 
 ########################################################################################################################
 # Build and run tests with no GPU
-# These request the "nogpu" license so we can manage oversubscription
+# These request the "nogpu" license so we can manage oversubscription of
+
 sbatch -o "run-logs/slurm-%j.out" \
 	-L nogpu:1 \
 	-p ubuntu-lts \
 	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh
+
+sbatch -o "run-logs/slurm-%j.out" \
+	-L nogpu:1 \
+	-p ubuntu-lts \
+	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu-sanitizer.sh
 
 sbatch -o "run-logs/slurm-%j.out" \
 	-L nogpu:1 \
@@ -42,6 +53,7 @@ sbatch -o "run-logs/slurm-%j.out" \
 # Build and run tests that execute on the Mac Mini
 # These request the "macmini" license so we can limit the number of jobs on the host,
 # which has both a hypervisor-enforced 2-instance license limit, and only 16GB of RAM
+
 sbatch -o "run-logs/slurm-%j.out" \
 	-L macmini:1 \
 	-p macos\
