17c6509db7bef457f7e9a5c52a51717feb52a1be
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-05-12 21:38:09 -0700

    Propagate commit information during checkout

--- ci-jobs/job-macos.sh
+++ ci-jobs/job-macos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+export BRANCH=$1
+export COMMIT=$2
+
 cd /Users/ci/Documents
 git clone --recursive https://github.com/ngscopeclient/scopehal-apps
 cd scopehal-apps
+git checkout $COMMIT
 ./test-scripts/test-driver-macos.sh
--- ci-jobs/job-windows.sh
+++ ci-jobs/job-windows.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export BRANCH=$1
+export COMMIT=$2
+
 git clone --recursive https://github.com/ngscopeclient/scopehal-apps
 cd scopehal-apps
+git checkout $COMMIT
 ./test-scripts/test-driver-windows.sh
