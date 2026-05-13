3a964654c7699bafc2249c7cfa56fabad83fe82f
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-05-12 22:22:59 -0700

    Added output redirection to all sbatch calls

--- batch-launcher.sh
+++ batch-launcher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ci
+export PATH=/usr/local/bin:/usr/bin:/bin:/home/ci/scopehal-ci-scripts/vm
 
 ########################################################################################################################
 # Build and run tests using GPUs
@@ -7,27 +8,22 @@ cd /home/ci
 # so jobs from a future block can begin running once the job on their GPU from the previous block completes,
 # without any need to wait for other jobs in the block
 
-echo "hello world";
-
 sbatch -o "run-logs/slurm-%j.out" \
 	-L nvidia1630_18:1,sanquentin:1 \
 	-p win11 \
 	--time=45 \
 	run-task-msys scopehal-ci-scripts/ci-jobs/job-windows.sh 2>&1
 
-echo "foobar";
-exit;
-
 sbatch -o "run-logs/slurm-%j.out" \
 	-L nvidia1630_51:1,sanquentin:1 \
 	-p debian-oldstable \
 	--time=45 \
-	run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
+	run-task scopehal-ci-scripts/ci-jobs/job-debian.sh 2>&1
 sbatch -o "run-logs/slurm-%j.out" \
 	-L nvidia1630_8a:1,sanquentin:1 \
 	-p ubuntu-oldlts \
 	--time=45 \
-	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh
+	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh 2>&1
 
 ##
 
@@ -35,12 +31,12 @@ sbatch -o "run-logs/slurm-%j.out" \
 	-L nvidia1630_51:1,sanquentin:1 \
 	-p arch \
 	--time=45 \
-	run-task scopehal-ci-scripts/ci-jobs/job-arch.sh
+	run-task scopehal-ci-scripts/ci-jobs/job-arch.sh 2>&1
 sbatch -o "run-logs/slurm-%j.out" \
 	-L nvidia1630_8a:1,sanquentin:1 \
 	-p debian-stable \
 	--time=45 \
-	run-task scopehal-ci-scripts/ci-jobs/job-debian.sh
+	run-task scopehal-ci-scripts/ci-jobs/job-debian.sh 2>&1
 
 ########################################################################################################################
 # Build and run tests with no GPU
@@ -50,25 +46,25 @@ sbatch -o "run-logs/slurm-%j.out" \
 	-L sanquentin:1 \
 	-p ubuntu-lts \
 	--time=45 \
-	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh
+	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu.sh 2>&1
 
 sbatch -o "run-logs/slurm-%j.out" \
 	-L sanquentin:1 \
 	-p ubuntu-lts \
 	--time=45 \
-	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu-sanitizer.sh
+	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu-sanitizer.sh 2>&1
 
 sbatch -o "run-logs/slurm-%j.out" \
 	-L sanquentin:1 \
 	-p ubuntu-lts \
 	--time=45 \
-	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu-analyze.sh
+	run-task scopehal-ci-scripts/ci-jobs/job-ubuntu-analyze.sh 2>&1
 
 sbatch -o "run-logs/slurm-%j.out" \
 	-L sanquentin:1 \
 	-p fedora \
 	--time=45 \
-	run-task scopehal-ci-scripts/ci-jobs/job-fedora.sh
+	run-task scopehal-ci-scripts/ci-jobs/job-fedora.sh 2>&1
 
 ########################################################################################################################
 # Build and run tests that execute on the Mac Mini
@@ -79,4 +75,4 @@ sbatch -o "run-logs/slurm-%j.out" \
 	-L macmini:1 \
 	-p macos\
 	--time=45 \
-	run-task-macos scopehal-ci-scripts/ci-jobs/job-macos.sh
+	run-task-macos scopehal-ci-scripts/ci-jobs/job-macos.sh 2>&1
