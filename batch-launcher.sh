4636e7e412e8a58350a51347d5f88efe2c0a20e1
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-05-14 06:37:14 -0700

    Changed nvidia1630 to 3050 for slot 8a

--- batch-launcher.sh
+++ batch-launcher.sh
@@ -18,11 +18,11 @@ export NODE_EXTRA_CA_CERTS=/etc/ssl/certs/antikernel-root-2026.crt
 
 JOB0=`sbatch-wrapper.sh -L nvidia1630_18:1,sanquentin:1 -p win11 run-task-msys job-windows.sh`
 JOB1=`sbatch-wrapper.sh -L nvidia1630_51:1,sanquentin:1 -p debian-oldstable run-task job-debian.sh`
-JOB2=`sbatch-wrapper.sh -L nvidia1630_8a:1,sanquentin:1 -p ubuntu-oldlts run-task job-ubuntu.sh`
+JOB2=`sbatch-wrapper.sh -L nvidia3050_8a:1,sanquentin:1 -p ubuntu-oldlts run-task job-ubuntu.sh`
 
 ##
 JOB3=`sbatch-wrapper.sh -L nvidia1630_51:1,sanquentin:1 -p arch run-task job-arch.sh`
-JOB4=`sbatch-wrapper.sh -L nvidia1630_8a:1,sanquentin:1 -p debian-stable run-task job-debian.sh`
+JOB4=`sbatch-wrapper.sh -L nvidia3050_8a:1,sanquentin:1 -p debian-stable run-task job-debian.sh`
 
 ########################################################################################################################
 # Build and run tests with no GPU
