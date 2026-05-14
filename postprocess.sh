fdd66f42fc7b404be059d10c8834b406544975a4
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-05-14 10:37:25 -0700

    copy slurm log to buildlog.txt so it displays without downloading

--- postprocess.sh
+++ postprocess.sh
@@ -44,13 +44,13 @@ scp artifacts/$FEDORA_JOB/* ci@$FILE_HOST:$OUTBASE/fedora-43-amd64/
 scp artifacts/$MACOS_JOB/* ci@$FILE_HOST:$OUTBASE/macos-15-6-arm64/
 
 # Push build logs for debugging
-scp run-logs/slurm-$WIN11_JOB.out ci@$FILE_HOST:$OUTBASE/win11-x64/
-scp run-logs/slurm-$DEBIAN_OLDSTABLE_JOB.out ci@$FILE_HOST:$OUTBASE/debian-12-amd64/
-scp run-logs/slurm-$UBUNTU_OLDLTS_JOB.out ci@$FILE_HOST:$OUTBASE/ubuntu-24-04-amd64/
-scp run-logs/slurm-$ARCH_JOB.out ci@$FILE_HOST:$OUTBASE/arch-amd64/
-scp run-logs/slurm-$DEBIAN_STABLE_JOB.out ci@$FILE_HOST:$OUTBASE/debian-13-amd64/
-scp run-logs/slurm-$UBUNTU_LTS_JOB.out ci@$FILE_HOST:$OUTBASE/ubuntu-26-04-amd64/
-scp run-logs/slurm-$FEDORA_JOB.out ci@$FILE_HOST:$OUTBASE/fedora-43-amd64/
-scp run-logs/slurm-$MACOS_JOB.out ci@$FILE_HOST:$OUTBASE/macos-15-6-arm64/
+scp run-logs/slurm-$WIN11_JOB.out ci@$FILE_HOST:$OUTBASE/win11-x64/buildlog.txt
+scp run-logs/slurm-$DEBIAN_OLDSTABLE_JOB.out ci@$FILE_HOST:$OUTBASE/debian-12-amd64/buildlog.txt
+scp run-logs/slurm-$UBUNTU_OLDLTS_JOB.out ci@$FILE_HOST:$OUTBASE/ubuntu-24-04-amd64/buildlog.txt
+scp run-logs/slurm-$ARCH_JOB.out ci@$FILE_HOST:$OUTBASE/arch-amd64/buildlog.txt
+scp run-logs/slurm-$DEBIAN_STABLE_JOB.out ci@$FILE_HOST:$OUTBASE/debian-13-amd64/buildlog.txt
+scp run-logs/slurm-$UBUNTU_LTS_JOB.out ci@$FILE_HOST:$OUTBASE/ubuntu-26-04-amd64/buildlog.txt
+scp run-logs/slurm-$FEDORA_JOB.out ci@$FILE_HOST:$OUTBASE/fedora-43-amd64/buildlog.txt
+scp run-logs/slurm-$MACOS_JOB.out ci@$FILE_HOST:$OUTBASE/macos-15-6-arm64/buildlog.txt
 
 # TODO: clean up local copies of artifacts and build logs
