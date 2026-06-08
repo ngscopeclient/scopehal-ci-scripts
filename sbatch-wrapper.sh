dd6e6dcd0fb21e39a42db58bdf2a46b9d7a123f5
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-06-08 08:30:49 -0700

    Fixed missing quote

--- sbatch-wrapper.sh
+++ sbatch-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SHORTCOMMIT=`echo $COMMIT | cut -c 1-6`
-sbatch -o "run-logs/slurm-%j.out" -J "$BRANCH-$SHORTCOMMIT --time=60 "$@" | cut -d " " -f 4
+sbatch -o "run-logs/slurm-%j.out" -J "$BRANCH-$SHORTCOMMIT" --time=60 "$@" | cut -d " " -f 4
